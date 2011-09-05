24dadd8cab0b1ce4c638232382ceb6687adbd5d1
DEMO <DEMO@DEMO-LAP>
2011-09-05 19:42:38 +0800

    j

--- src/wrp/alog/sort/BubbleSort.java
+++ src/wrp/alog/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package wrp.alog.sort;
 
+import java.util.Random;
+
 public class BubbleSort {
 
 	/**
@@ -7,7 +9,11 @@ public class BubbleSort {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int[] intArr = new int[50];
+		Random random = new Random(100);
+		for(int i=0; i<50; i++){
+			intArr[i] = random.nextInt(300);
+		}
 	}
-
 }
