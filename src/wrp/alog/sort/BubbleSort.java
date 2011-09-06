64c0b9d0634651eb605f8ffc9c8f1e7429538b2a
DEMO <DEMO@DEMO-LAP>
2011-09-06 22:39:05 +0800

    complete bubblesort

--- src/wrp/alog/sort/BubbleSort.java
+++ src/wrp/alog/sort/BubbleSort.java
@@ -10,10 +10,36 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] intArr = new int[50];
+		int[] intArr = new int[10];
 		Random random = new Random(100);
-		for(int i=0; i<50; i++){
+		for(int i=0; i<10; i++){
 			intArr[i] = random.nextInt(300);
+		}
+		
+		System.out.print("排序前：");
+		printarr(intArr);
+		
+		System.out.println();
+		for(int j=intArr.length-1; j>0; j--)
+			for(int i=0; i < j; i++){
+				swap(intArr, i);
+			}
+		
+		System.out.print("排序后：");
+		printarr(intArr);
+	}
+	
+	public static void swap(int[] intArr, int i){
+		if(intArr[i] > intArr[i+1]){
+			int temp = intArr[i];
+			intArr[i] = intArr[i+1];
+			intArr[i+1] = temp;
+		}
+	}
+	
+	public static void printarr(int[] intarr){
+		for(int i=0; i<intarr.length; i++){
+			System.out.print(intarr[i]+",");
 		}
 	}
 }
