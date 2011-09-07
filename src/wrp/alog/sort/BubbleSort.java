f5f60f14ef589732810e41234747c60240ca58ff
demo <demo@tyky-PC>
2011-09-07 09:40:56 +0800

    123

--- src/wrp/alog/sort/BubbleSort.java
+++ src/wrp/alog/sort/BubbleSort.java
@@ -7,6 +7,8 @@ public class BubbleSort {
 	/**
 	 * @param args
 	 */
+	public static int temp;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -14,6 +16,22 @@ public class BubbleSort {
 		Random random = new Random(100);
 		for(int i=0; i<50; i++){
 			intArr[i] = random.nextInt(300);
+		}
+		
+//		for(int i=0; i<50; i++){
+//			xswap(intArr, i);
+//		}
+		
+		int[] testarr = {23, 24};
+		xswap(testarr, 0);
+		System.out.println(testarr[0]+":"+ testarr[1]);
+	}
+	
+	public static void xswap(int[] intArr, int i){
+		if(intArr[i] > intArr[i+1]){
+			temp = intArr[i];
+			intArr[i] = intArr[i+1];
+			intArr[i+1] = temp;
 		}
 	}
 }
